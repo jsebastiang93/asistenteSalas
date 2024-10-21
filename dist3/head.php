f6102488661a6fb01a6ea0efcff643224c66d085
valen10210528 <valen05282126@gmail.com>
2024-10-20 23:54:28 -0500

    new

---
+++ dist3/head.php
@@ -0,0 +1,42 @@
+<?php
+
+$b_title = "ASIS SALAS 3";
+$b_description = "";
+$b_keywords = "";
+$b_url = " ";
+$b_url_business = "";
+$b_logo = "";
+$b_favicon = "assets/media/logos/LogoAsisSalas.ico";
+$b_icon = "assets/media/logos/icono.png";
+$b_color_p = "#0764A9";
+$b_business_name = "";
+$b_business_id = "";
+
+
+?>
+
+		<base href="./">
+		<meta charset="UTF-8">
+		<title><?php echo  $b_title ?></title>
+		<meta name="description" content="<?php echo $b_description ?>" />
+		<meta name="keywords" content="<?php echo $b_keywords ?>" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<meta charset="utf-8" />
+		<meta property="og:locale" content="es_ES" />
+		<meta property="og:type" content="website" />
+		<meta property="og:title" content="<?php echo  $b_title ?>" />
+		<meta property="og:url" content="<?php echo  $b_url ?>" />
+		<meta property="og:site_name" content="<?php echo $b_description ?>" />
+		<meta property="og:image" content="<?php echo $b_logo ?>" />
+		<link rel="canonical" href="<?php echo  $b_url ?>" />
+		<link rel="shortcut icon" href="<?php echo  $b_favicon ?>" />
+		<!--begin::Fonts-->
+		<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" />
+		<!--end::Fonts-->
+		<!--begin::Page Vendor Stylesheets(used by this page)-->
+		<link href="assets/plugins/custom/leaflet/leaflet.bundle.css" rel="stylesheet" type="text/css" />
+		<!--end::Page Vendor Stylesheets-->
+		<!--begin::Global Stylesheets Bundle(used by all pages)-->
+		<link href="assets/plugins/global/plugins.bundle.css" rel="stylesheet" type="text/css" />
+		<link href="assets/css/style.bundle.css" rel="stylesheet" type="text/css" />
+		<!--end::Global Stylesheets Bundle-->
---
+++ dist3/src/c_.php
@@ -0,0 +1,11 @@
+<?php
+
+	setlocale(LC_TIME, "spanish");
+
+	date_default_timezone_set('America/Bogota');
+
+	$name_page = "ASIS SALAS";
+	
+	$desc_page = "By ASISTENTE SALAS";
+
+?>
